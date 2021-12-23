90690aa7f47c895c73f27aac7c1aefd46b0a937c
Mensch272 <47662901+mensch272@users.noreply.github.com>
2021-12-23 16:54:42 +0500

    Basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -51,130 +51,171 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text('Chapturn'),
       ),
-      body: Container(
-        alignment: Alignment.center,
-        child: FractionallySizedBox(
-          widthFactor: 0.88,
-          child: ListView(
-            padding: const EdgeInsets.symmetric(vertical: 24.0),
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    flex: 7,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Container(
-                          height: 324,
-                          child: Card(
-                            child: Padding(
-                              padding: const EdgeInsets.all(12.0),
-                              child: Row(
-                                children: [
-                                  AspectRatio(
-                                    aspectRatio: 2 / 3,
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(10),
-                                      child: Image.asset(
-                                        'assets/images/abstract.jpg',
-                                        fit: BoxFit.fill,
-                                      ),
-                                    ),
-                                  ),
-                                  const SizedBox(width: 12),
-                                  Expanded(
-                                    child: Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                          vertical: 8.0),
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            'Some where nice, novel',
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .headline5,
-                                          ),
-                                          const SizedBox(height: 12.0),
-                                          Text(
-                                            'by bludeauth',
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .subtitle1,
-                                          ),
-                                          const SizedBox(height: 8.0),
-                                          Text('Completed • 8 chapters'),
-                                          const Spacer(),
-                                          ElevatedButton(
-                                            onPressed: () {},
-                                            child: Text('Download as epub'),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    flex: 3,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Card(
-                          child: Padding(
-                            padding: const EdgeInsets.all(12.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'About this',
-                                  style: Theme.of(context).textTheme.headline6,
-                                ),
-                                const SizedBox(height: 12.0),
-                                Text(
-                                  'Description',
-                                  style: Theme.of(context).textTheme.subtitle1,
-                                ),
-                                const SizedBox(height: 4.0),
-                                Text(
-                                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget ligula augue. Integer ligula risus, suscipit ut risus nec, interdum fermentum ante. Aliquam tempor erat.',
-                                ),
-                                const SizedBox(height: 8.0),
-                                Text(
-                                  'Genres',
-                                  style: Theme.of(context).textTheme.subtitle1,
-                                ),
-                                const SizedBox(height: 4.0),
-                                Wrap(
-                                  children: [Chip(label: Text('Fantasy'))],
-                                ),
-                                Center(
-                                  child: TextButton(
-                                    onPressed: () {},
-                                    child: const Text('Show More'),
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(
+          vertical: 24.0,
+          horizontal: MediaQuery.of(context).size.width * 0.06,
         ),
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                flex: 7,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Container(
+                      height: 324,
+                      child: Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Row(
+                            children: [
+                              AspectRatio(
+                                aspectRatio: 2 / 3,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: Image.asset(
+                                    'assets/images/abstract.jpg',
+                                    fit: BoxFit.fill,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 12),
+                              Expanded(
+                                child: Padding(
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: 8.0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Some where nice, novel',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .headline5,
+                                      ),
+                                      const SizedBox(height: 12.0),
+                                      Text(
+                                        'by bludeauth',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .subtitle1,
+                                      ),
+                                      const SizedBox(height: 8.0),
+                                      Text('Completed • 8 chapters'),
+                                      const SizedBox(height: 8.0),
+                                      Text('English'),
+                                      const Spacer(),
+                                      ElevatedButton(
+                                        onPressed: () {},
+                                        child: Text('Download as epub'),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    Card(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Text(
+                              '8 chapters',
+                              style: Theme.of(context).textTheme.headline6,
+                            ),
+                          ),
+                          CheckboxListTile(
+                            title: Text('Chapter 1'),
+                            subtitle: Text('unknown'),
+                            secondary: Icon(Icons.download_done),
+                            value: false,
+                            onChanged: (value) {},
+                          ),
+                          CheckboxListTile(
+                            title: Text('Chapter 1'),
+                            subtitle: Text(
+                              '14/12/2020',
+                            ),
+                            secondary: Icon(Icons.downloading),
+                            value: false,
+                            onChanged: (value) {},
+                          ),
+                          CheckboxListTile(
+                            title: Text('Chapter 1'),
+                            subtitle: Text(
+                              '14/12/2020',
+                            ),
+                            secondary: Icon(Icons.download),
+                            value: false,
+                            onChanged: (value) {},
+                          ),
+                          const SizedBox(height: 12),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(width: 12),
+              Expanded(
+                flex: 3,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'About this',
+                              style: Theme.of(context).textTheme.headline6,
+                            ),
+                            const SizedBox(height: 12.0),
+                            Text(
+                              'Description',
+                              style: Theme.of(context).textTheme.subtitle1,
+                            ),
+                            const SizedBox(height: 4.0),
+                            Text(
+                              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget ligula augue. Integer ligula risus, suscipit ut risus nec, interdum fermentum ante. Aliquam tempor erat.',
+                            ),
+                            const SizedBox(height: 8.0),
+                            Text(
+                              'Genres',
+                              style: Theme.of(context).textTheme.subtitle1,
+                            ),
+                            const SizedBox(height: 4.0),
+                            Wrap(
+                              children: [Chip(label: Text('Fantasy'))],
+                            ),
+                            Center(
+                              child: TextButton(
+                                onPressed: () {},
+                                child: const Text('Show More'),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
@@ -192,11 +233,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
         landscapeLayout: BottomNavigationBarLandscapeLayout.centered,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }

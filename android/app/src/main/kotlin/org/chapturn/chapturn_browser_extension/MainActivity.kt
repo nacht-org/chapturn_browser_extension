a9a5594f28622328f725a3e8af35b645d7680de0
Mensch272 <47662901+mensch272@users.noreply.github.com>
2021-12-23 14:58:19 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/org/chapturn/chapturn_browser_extension/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.chapturn.chapturn_browser_extension
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,203 @@
+import 'package:flutter/material.dart';
+import 'dart:html' as html;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        cardTheme: CardTheme(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chapturn'),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: FractionallySizedBox(
+          widthFactor: 0.88,
+          child: ListView(
+            padding: const EdgeInsets.symmetric(vertical: 24.0),
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    flex: 7,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Container(
+                          height: 324,
+                          child: Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Row(
+                                children: [
+                                  AspectRatio(
+                                    aspectRatio: 2 / 3,
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(10),
+                                      child: Image.asset(
+                                        'assets/images/abstract.jpg',
+                                        fit: BoxFit.fill,
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(width: 12),
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 8.0),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            'Some where nice, novel',
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .headline5,
+                                          ),
+                                          const SizedBox(height: 12.0),
+                                          Text(
+                                            'by bludeauth',
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .subtitle1,
+                                          ),
+                                          const SizedBox(height: 8.0),
+                                          Text('Completed • 8 chapters'),
+                                          const Spacer(),
+                                          ElevatedButton(
+                                            onPressed: () {},
+                                            child: Text('Download as epub'),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'About this',
+                                  style: Theme.of(context).textTheme.headline6,
+                                ),
+                                const SizedBox(height: 12.0),
+                                Text(
+                                  'Description',
+                                  style: Theme.of(context).textTheme.subtitle1,
+                                ),
+                                const SizedBox(height: 4.0),
+                                Text(
+                                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget ligula augue. Integer ligula risus, suscipit ut risus nec, interdum fermentum ante. Aliquam tempor erat.',
+                                ),
+                                const SizedBox(height: 8.0),
+                                Text(
+                                  'Genres',
+                                  style: Theme.of(context).textTheme.subtitle1,
+                                ),
+                                const SizedBox(height: 4.0),
+                                Wrap(
+                                  children: [Chip(label: Text('Fantasy'))],
+                                ),
+                                Center(
+                                  child: TextButton(
+                                    onPressed: () {},
+                                    child: const Text('Show More'),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Book',
+            icon: Icon(Icons.book),
+          ),
+          BottomNavigationBarItem(
+            label: 'Browse',
+            icon: Icon(Icons.compass_calibration),
+          ),
+          BottomNavigationBarItem(
+            label: 'More',
+            icon: Icon(Icons.more_horiz),
+          ),
+        ],
+        landscapeLayout: BottomNavigationBarLandscapeLayout.centered,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}

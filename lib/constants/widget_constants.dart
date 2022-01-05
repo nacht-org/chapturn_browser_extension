1da2ee0e596ae5585ff339d6d2066271e4d13441
Mensch272 <47662901+mensch272@users.noreply.github.com>
2022-01-05 11:13:17 +0500

    Popup WIP

--- lib/constants/widget_constants.dart
+++ lib/constants/widget_constants.dart
@@ -4,3 +4,4 @@ const kContentPadding = 12.0;
 const kSplashRadius = 24.0;
 const kPreferredCardWidth = 500.0;
 const kRadius = 10.0;
+const kChipSpacing = kContentPadding / 2;
--- lib/core/popup/pages/popup.dart
+++ lib/core/popup/pages/popup.dart
@@ -2,8 +2,10 @@ import 'package:chapturn_browser_extension/constants/widget_constants.dart';
 import 'package:chapturn_browser_extension/core/novel/providers.dart';
 import 'package:chapturn_browser_extension/core/popup/widgets/crawler_card.dart';
 import 'package:chapturn_browser_extension/widgets/modal_card.dart';
+import 'package:chapturn_sources/src/models/meta.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class PopupHome extends HookConsumerWidget {
@@ -17,30 +19,134 @@ class PopupHome extends HookConsumerWidget {
     var theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: theme.colorScheme.surface,
+        backgroundColor: Colors.transparent,
         titleTextStyle: theme.textTheme.headline6?.copyWith(
           fontWeight: FontWeight.bold,
         ),
         iconTheme: IconThemeData(
           color: theme.colorScheme.onSurface,
         ),
-        elevation: 2,
+        elevation: 0,
         title: const Text('Chapturn'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.brightness_auto),
+            splashRadius: kSplashRadius,
+            tooltip: 'Switch theme',
+          ),
+          const SizedBox(width: kContentPadding),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.settings),
+            splashRadius: kSplashRadius,
+            tooltip: 'Open settings',
+          ),
+        ],
       ),
-      body: const SafeArea(child: PopupList()),
+      body: const SafeArea(child: PopupView()),
     );
   }
 }
 
-class PopupList extends ConsumerWidget {
-  const PopupList({Key? key}) : super(key: key);
+class PopupView extends ConsumerWidget {
+  const PopupView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ListView(
-      padding: const EdgeInsets.symmetric(vertical: kRadius),
-      children: const [
-        CrawlerCard(),
-      ],
+    final crawlerState = ref.watch(crawlerController);
+
+    return crawlerState.when(
+      loading: () => const LoadingCard(),
+      fetching: (url, meta) => const LoadingCard(
+        heading: 'Fetching...',
+      ),
+      data: (meta, crawler, novel) => Container(),
+      supported: (url, meta) {
+        return SupportedCard(url, meta);
+      },
+      unsupported: (url, meta) => const Text('Unsupported'),
+      error: (e) => Text(e.toString()),
+    );
+  }
+}
+
+class SupportedCard extends StatelessWidget {
+  const SupportedCard(
+    this.url,
+    this.meta, {
+    Key? key,
+  }) : super(key: key);
+
+  final String url;
+  final Meta meta;
+
+  @override
+  Widget build(BuildContext context) {
+    final uri = Uri.parse(url);
+
+    return Card(
+      margin: const EdgeInsets.all(kNormalPadding),
+      child: Padding(
+        padding: const EdgeInsets.all(kNormalPadding),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              children: [
+                Text(
+                  uri.host,
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+              ],
+            ),
+            Text(
+              'This site is supported',
+              style: Theme.of(context).textTheme.caption,
+            ),
+            const SizedBox(height: kContentPadding),
+            Wrap(
+              spacing: kChipSpacing,
+              children: [
+                Chip(label: Text(meta.lang)),
+                for (var dir in meta.readingDirections)
+                  Chip(label: Text(dir.name)),
+                Chip(label: Text(meta.updated.toString())),
+              ],
+            ),
+            const SizedBox(height: kNormalPadding),
+            Text(
+              'Actions',
+              style: Theme.of(context).textTheme.overline,
+            ),
+            const SizedBox(height: kContentPadding),
+            ListTile(
+              onTap: () {},
+              leading: Icon(Icons.web),
+              title: Text('Continue'),
+              dense: true,
+            ),
+            ListTile(
+              onTap: () {},
+              leading: Icon(Icons.tab),
+              title: Text('Open in tab mode'),
+              dense: true,
+            ),
+            const SizedBox(height: kContentPadding),
+            Text(
+              'Options',
+              style: Theme.of(context).textTheme.overline,
+            ),
+            const SizedBox(height: kContentPadding),
+            CheckboxListTile(
+              value: false,
+              onChanged: (value) {},
+              title: const Text('Remember my choice'),
+              dense: true,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
